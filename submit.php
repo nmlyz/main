90b19a6aae83e867c7de3f77d852dd8f326f6a24
zen0831 <84489715+zen0831@users.noreply.github.com>
2022-02-20 15:56:34 +0900

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,45 +1,35 @@
 <?php
-// 入力内容の取得・変数に格納
-$name    = $_POST['name'];     // 氏名
-$to      = $_POST['to'];       // メールアドレス
-$subject = $_POST['subject'];  // 件名
-$message = $_POST['message'];  // 本文
 
-// メール日本語対応
-mb_language("japanese");
-mb_internal_encoding("UTF-8");
+/**
+ * This example shows sending a message using PHP's mail() function.
+ */
 
-// PHPMailer クラスをネーム空間にインポート
+//Import the PHPMailer class into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-// Composer の autoloader をロード
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
-// インスタンス生成
-$mail = new PHPMailer(true);
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('whoto@example.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer mail() test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
 
-try {
-    // SMTPの設定
-    $mail->isSMTP();                       // SMTP 利用
-    $mail->Host       = 'smtp.gmail.com';  // SMTP サーバー(Gmail の場合これ)
-    $mail->SMTPAuth   = true;              // SMTP認証を有効にする
-    $mail->Username   = 'zenzenzen087@gmail.com'; // ユーザ名 (Gmail ならメールアドレス)
-    $mail->Password   = 'zennago0831';      // パスワード
-    $mail->SMTPSecure = 'tls';             // 暗号化通信 (Gmail では使えます)
-    $mail->Port       = 587;               // TCP ポート (TLS の場合 587)
-
-    // メール本体
-    $mail->setFrom('zenzenzen087@gmail.com', 'zen');  // 送信元メールアドレスと名前
-    $mail->addAddress($to, mb_encode_mimeheader($name, 'ISO-2022-JP'));  // 送信先メールアドレスと名前
-    $mail->Subject = mb_encode_mimeheader($subject, 'ISO-2022-JP');  // 件名
-    $mail->Body    = mb_convert_encoding($message, "JIS","UTF-8");  // 本文
-
-// 送信
-    $mail->send();
-    echo '送信済み';
-} catch (Exception $e) {
-    echo "送信失敗: {$mail->ErrorInfo}";
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
-?>
